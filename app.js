cb6ffc74b9d3bc601a10ce1750d9df2d5d339c30
estefSolis <estefanymichelle2@gmail.com>
2025-09-12 12:08:52 -0600

    Funcion agregarAmigo y cargarLista

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+let amigos = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById('amigo');
+    let amigo = inputAmigo.value;
+    if(amigo === ""){
+        alert("Por favor, inserte un nombre.");
+        return;
+    }else{
+        amigos.push(amigo);
+        cargarLista();
+        console.log(amigos);
+    }
+}
+
+function cargarLista(){
+    let lista = document.getElementById('listaAmigos');
+
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        lista.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+    return;
+}
+
+cargarLista();
 
